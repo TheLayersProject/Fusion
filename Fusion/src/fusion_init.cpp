bca7c9f2a43d53b8becf667e25bfa5475348a843
Hunter Malm <huntrmalm@gmail.com>
2025-03-20 20:43:27 -0400

    Publishing and loading definitions internally
    - Definitions are now included with the source code.
    - Definitions are compiled and loaded using the new Layers resource
      system.

--- Fusion/src/fusion_init.cpp
+++ Fusion/src/fusion_init.cpp
@@ -18,14 +18,35 @@
  */
 
 #include <Fusion/fusion_init.h>
+#include <Fusion/fusion_global.h>
+
+#include <QDebug>
 
 #include <Layers/lstring.h>
 #include <Layers/lcontroller.h>
 
-namespace Fusion {
-    Initializer::Initializer() {
-        lController.include("Fusion (The Layers Project)/0.1.0");
+namespace Layers {
+    namespace Resources {
+        extern void initResources_definitions();
     }
-
-    static Initializer fusion_initializer;
 }
+
+void initialize_resources()
+{
+    // Initialize Layers resources
+    Layers::Resources::initResources_definitions();
+}
+
+FUSION_NAMESPACE_BEGIN
+
+Initializer::Initializer()
+{
+    initialize_resources();
+
+    lController.include_internal("/definitions/fusion");
+    qDebug() << "Fusion: Loaded Definitions";
+}
+
+static Initializer fusion_initializer;
+
+FUSION_NAMESPACE_END
